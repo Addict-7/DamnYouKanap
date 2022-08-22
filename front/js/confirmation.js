e99193cfc8b57dabc1afd6e0fa4e95ae369cd73e
ELRLHZ <gicss27@hotmail.com>
2022-08-22 11:29:27 +0200

    Commentaires

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,7 @@
 // Récupère les différents paramètres de l'URL actuelle
 let params = new URLSearchParams(document.location.search);
-// Accède au paramètre "id" (http://127.0.0.1:5500/front/html/prduct.html?id=15684512331)
+
+// Accède au paramètre "id" (http://127.0.0.1:5500/front/html/confirmation.html?id=15684512331)
 let id = params.get("id");
 
 // Affiche le numéro de commande
